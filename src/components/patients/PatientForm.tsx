c65ead2669c9ad3a85d8097da801cfdf492926ad
Tabi Idris <tabidrizy@gmail.com>
2023-05-15 18:46:10 +0100

    removed unused imports from patient components

--- src/components/patients/PatientForm.tsx
+++ src/components/patients/PatientForm.tsx
@@ -1,12 +1,8 @@
 import { useTranslation } from 'react-i18next';
-import { UploadOutlined, InboxOutlined } from '@ant-design/icons';
 import React, { useState } from 'react';
 import { BaseButtonsForm } from '@app/components/common/forms/BaseButtonsForm/BaseButtonsForm';
-import { InputNumber } from '@app/components/common/inputs/InputNumber/InputNumber';
 import { BaseSelect, Option } from '@app/components/common/selects/BaseSelect/BaseSelect';
 import { BaseButton } from '@app/components/common/BaseButton/BaseButton';
-import { BaseUpload } from '@app/components/common/BaseUpload/BaseUpload';
-import { BaseRate } from '@app/components/common/BaseRate/BaseRate';
 import { notificationController } from '@app/controllers/notificationController';
 import { BaseInput } from '@app/components/common/inputs/BaseInput/BaseInput';
 import { Col, DatePicker, Row, TimePicker } from 'antd';
